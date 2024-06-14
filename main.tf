3bd3a60476f8761f9a2de87c7226d92eed968d2f
milindtailor <168051482+milindtailor@users.noreply.github.com>
2024-06-14 08:49:08 +0530

    Update main capslock.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
         #profile = "default"
 	}
 
-resource "aws_instance" "MilindInfra" {
+resource "aws_instance" "milindinfra" {
 	ami = "ami-0c2b8ca1dad447f8a" 
 	instance_type = "t2.micro"
 }
